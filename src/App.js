5bd3a9379aea4c4196299e3abc62678dd4256670
uni51 <unieye51@gmail.com>
2023-10-09 14:02:05 +0900

    3-4-2. axiosを利用した場合

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Todo from "./components/Todo";
 import "./App.css";
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <div className="App">
-      <Todo />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <Todo />
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+const fetchTodoList = async () => {
+  const res = await axios.get("http://localhost:3001/todos");
+  return res.data;
+};
+
 const Todo = () => {
+  const {
+    isLoading,
+    isError,
+    data: todos,
+    error,
+  } = useQuery(["todos"], fetchTodoList);
+
+  if (isLoading) {
+    return <span>Loading...</span>;
+  }
+
+  if (isError) {
+    console.log(error);
+    return <span>Error: {error.response.data.message}</span>;
+  }
+
   return (
     <>
       <h1>Todo一覧</h1>
+      <ul>
+        {todos?.map((todo) => (
+          <li key={todo.id}>{todo.name}</li>
+        ))}
+      </ul>
     </>
   );
 };
