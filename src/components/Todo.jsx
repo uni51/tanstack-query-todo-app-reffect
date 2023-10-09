091957ca9e023e558064d6ed9f5ea1ad3667ccb0
uni51 <unieye51@gmail.com>
2023-10-09 17:19:06 +0900

    コメント追加

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,12 +1,109 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
 
 const fetchTodoList = async () => {
-  const res = await axios.get("http://localhost:3001/todos");
-  return res.data;
+  const res = await fetch("http://localhost:3001/todos");
+  if (!res.ok) {
+    throw new Error(`${res.status} ${res.statusText}`);
+  }
+  return res.json();
 };
 
 const Todo = () => {
+  const [name, setName] = useState("");
+  const queryClient = useQueryClient();
+
+  const addTodo = async (todo) => {
+    const res = await fetch("http://localhost:3001/todos/create", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    return res.json();
+  };
+
+  const deleteTodo = async (id) => {
+    const res = await fetch(`http://localhost:3001/todos/${id}`, {
+      method: "DELETE",
+    });
+    if (!res.ok) {
+      console.log("res", res);
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    return res.json();
+  };
+
+  const updateTodo = async (todo) => {
+    const res = await fetch(`http://localhost:3001/todos/${todo.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(todo),
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    return res.json();
+  };
+
+  const addMutation = useMutation(addTodo, {
+    onMutate: async (todo) => {
+      await queryClient.cancelQueries(["todos"]);
+
+      // Snapshot the previous value
+      const previousTodos = queryClient.getQueryData(["todos"]);
+
+      // Optimistically update to the new value
+      queryClient.setQueryData(["todos"], (old) => [...old, todo]);
+
+      // リクエストが失敗した時に onErrror で利用するために更新前のデータを返す
+      return { previousTodos };
+    },
+    onError: (err, todo, context) => {
+      queryClient.setQueryData(["todos"], context.previousTodos);
+    },
+    // リクエストの成功、失敗に関わらず実行される
+    onSettled: () => {
+      // 指定したキーのキャッシュを無効化し、useQueryを再実行する
+      queryClient.invalidateQueries("todos");
+    },
+  });
+
+  const deleteMutation = useMutation(deleteTodo, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("todos");
+    },
+  });
+
+  const updateMutation = useMutation(updateTodo, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("todos");
+    },
+  });
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addMutation.mutate({ name, isCompleted: false });
+  };
+
+  const handleRemoveTodo = (id) => {
+    deleteMutation.mutate(id);
+  };
+
+  const handleCheckChange = (todo) => {
+    updateMutation.mutate(todo);
+  };
+
   const {
     isLoading,
     isError,
@@ -19,16 +116,49 @@ const Todo = () => {
   }
 
   if (isError) {
-    console.log(error);
-    return <span>Error: {error.response.data.message}</span>;
+    return <span>Error: {error.message}</span>;
   }
 
   return (
     <>
       <h1>Todo一覧</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Add Todo :</label>
+          <input
+            placeholder="Add New Todo"
+            value={name}
+            onChange={handleChange}
+            id="name"
+          />
+          <button>追加</button>
+        </form>
+      </div>
       <ul>
         {todos?.map((todo) => (
-          <li key={todo.id}>{todo.name}</li>
+          <li
+            key={todo.id}
+            style={
+              todo.isCompleted === true
+                ? { textDecorationLine: "line-through" }
+                : {}
+            }
+          >
+            <input
+              type="checkbox"
+              checked={todo.isCompleted}
+              onChange={() =>
+                handleCheckChange({ ...todo, isCompleted: !todo.isCompleted })
+              }
+            />
+            {todo.name}
+            <button
+              style={{ marginLeft: "0.2em", cursor: "pointer" }}
+              onClick={() => handleRemoveTodo(todo.id)}
+            >
+              X
+            </button>
+          </li>
         ))}
       </ul>
     </>
